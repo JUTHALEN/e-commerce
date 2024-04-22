e07b4fbef430fb5ce68b67f129b878dce1968b3c
JUTHALEN <judith.alende@gmail.com>
2024-04-22 17:44:44 +0200

    Model creation

--- src/main/java/com/alende/judith/EcommerceApplication.java
+++ src/main/java/com/alende/judith/EcommerceApplication.java
@@ -1,11 +1,12 @@
 package com.alende.judith;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(EcommerceApplication.class, args);
     }
 }
